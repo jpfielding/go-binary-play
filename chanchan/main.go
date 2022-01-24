b92eca202038c7efdd478453a86b8470e344387f
JP Fielding <jp.fielding@gmail.com>
2022-01-24 01:15:19 +0000

    chan chan example for order result return

---
+++ chanchan/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// simple way to emulate ordered responses
+func main() {
+	all := make(chan chan int, 100)
+	for i := 0; i < 100; i++ {
+		chint := make(chan int)
+		all <- chint
+		go func(chint chan int, ii int) {
+			defer close(chint)
+			<-time.After(time.Millisecond * time.Duration(rand.Intn(1000)))
+			chint <- ii
+		}(chint, i)
+	}
+	close(all)
+	for chint := range all {
+		fmt.Printf("found %d\n", <-chint)
+	}
+}
