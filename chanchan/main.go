15ba53e1608858b6d502b6a192d9b962eda92172
JP Fielding <jp.fielding@strongdm.com>
2023-02-22 15:37:29 +0000

    chan chan update

--- chanchan/main.go
+++ chanchan/main.go
@@ -7,19 +7,23 @@ import (
 
 // simple way to emulate ordered responses
 func main() {
-	all := make(chan chan int, 100)
-	for i := 0; i < 100; i++ {
-		// do this outside the
-		chint := make(chan int)
-		all <- chint
-		go func(chint chan int, i int) {
-			defer close(chint)
-			<-time.After(5 * time.Second)
-			chint <- i
-		}(chint, i)
-	}
-	close(all)
-	for chint := range all {
+	now := time.Now()
+	defer func() { fmt.Printf("%v\n", time.Now().Sub(now)) }()
+	queue := make(chan chan int, 100)
+	go func() {
+		for i := 0; i < 100; i++ {
+			// do this outside the
+			chint := make(chan int)
+			queue <- chint
+			go func(chint chan int, i int) {
+				defer close(chint)
+				<-time.After(5 * time.Second)
+				chint <- i
+			}(chint, i)
+		}
+		close(queue)
+	}()
+	for chint := range queue {
 		now := time.Now()
 		i := <-chint
 		fmt.Printf("found %d %v\n", i, time.Since(now))
